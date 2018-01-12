53a1ec9efd21370af5c52506ae440e8530d7dbb6
Shawn Presser <shawnpresser@gmail.com>
2018-01-12 13:19:42 -0600

    Update message

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
-console.log('git rekt');
+console.log(
+  "Congratulations, you're using the shawwn-user-test feature branch."
+);
+console.log(
+  'Now revert to master: https://github.com/parcel-bundler/parcel/wiki/HEAD'
+);
+console.log('Parcel will now exit.');
 process.exit();
 
 // Node 8 supports native async functions - no need to use compiled code!
